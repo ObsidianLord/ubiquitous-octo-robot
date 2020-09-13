c065773376308e30a1eebc073230033be2385ee6
pihanya <gostevvm@yandex.ru>
2020-09-13 23:16:53 +0300

    Add additional details edit form

---
+++ src/panels/AdditionalDetailsEditForm.js
@@ -0,0 +1,133 @@
+import React from 'react';
+
+import {store} from '../store'
+import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
+import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
+import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
+import {IOS, platform} from "@vkontakte/vkui";
+import Icon28ChevronBack from "@vkontakte/icons/dist/28/chevron_back";
+import Icon24Back from "@vkontakte/icons/dist/24/back";
+import FormLayout from "@vkontakte/vkui/dist/components/FormLayout/FormLayout";
+import Input from "@vkontakte/vkui/dist/components/Input/Input";
+import Textarea from "@vkontakte/vkui/dist/components/Textarea/Textarea";
+import FixedLayout from "@vkontakte/vkui/dist/components/FixedLayout/FixedLayout";
+import Div from "@vkontakte/vkui/dist/components/Div/Div";
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
+import FormLayoutGroup from "@vkontakte/vkui/dist/components/FormLayoutGroup/FormLayoutGroup";
+import Radio from "@vkontakte/vkui/dist/components/Radio/Radio";
+import Select from "@vkontakte/vkui/dist/components/Select/Select";
+import Title from "@vkontakte/vkui/dist/components/Typography/Title/Title";
+
+const osName = platform();
+
+export default class AdditionalDetailsEditForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id: props.id,
+            go: props.go,
+            store: store,
+
+            availableAuthors: [
+                {value: 'm', title: 'Матвей Правосудов'},
+                {value: 'f', title: 'Женский'}
+            ],
+
+            endsDate: undefined,
+
+            shouldDisplayDatePicked: false
+        }
+        this.state.defaultAuthor = this.state.availableAuthors[0].value
+
+        this.onAuthorChosen = this.onAuthorChosen.bind(this)
+        this.onFundEndsChosen = this.onFundEndsChosen.bind(this)
+        this.onDateChosen = this.onDateChosen.bind(this)
+        this.dateToFormat = this.dateToFormat.bind(this)
+    }
+
+    componentDidMount() {
+    }
+
+    onAuthorChosen(event) {
+        const {name, value} = event.currentTarget;
+        this.state.availableAuthors.forEach(author => {
+            if (value === author.value) {
+                store.author = author.title
+            }
+        });
+    }
+
+    onFundEndsChosen(event, dateNotDefined) {
+        this.setState({
+            ...this.state,
+            shouldDisplayDatePicked: !dateNotDefined
+        })
+    }
+
+    onDateChosen(event) {
+        const {name, value} = event.currentTarget
+        console.log("onDateChosen")
+    }
+
+    dateToFormat(date) {
+        return date.getFullYear() + '-' + (date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-' + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate());
+    }
+
+    render() {
+        return (
+            <Panel id={this.state.id}>
+                <PanelHeader
+                    left={<PanelHeaderButton onClick={this.state.go} data-to="back">
+                        {osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+                    </PanelHeaderButton>}
+                >
+                    Дополнительно
+                </PanelHeader>
+                <FormLayout style={{paddingBottom: 60}}>
+
+                    <Select top="Автор"
+                            defaultValue={this.state.defaultAuthor}
+                            onChange={this.onAuthorChosen}
+                            value={this.state.author}
+                            name={"author"}
+                    >
+                        {
+                            this.state.availableAuthors.map(author =>
+                                (<option value={author.value}>{author.title}</option>)
+                            )
+                        }
+                        {/*<option value="m">Матвей Правосудов</option>
+                        <option value="f">Женский</option>*/}
+                    </Select>
+
+                    <FormLayoutGroup top="Сбор завершится">
+                        <Radio onChange={(event) => this.onFundEndsChosen(event, true)} name="type">Когда соберём
+                            сумму</Radio>
+                        <Radio onChange={(event) => this.onFundEndsChosen(event, false)} name="type">В определённую
+                            дату</Radio>
+                    </FormLayoutGroup>
+
+                    {
+                        this.state.shouldDisplayDatePicked &&
+                        <Input type="date"
+                               top="Сбор завершится"
+                               placeholder="Выберите дату"
+                               name={"dateEnds"}
+                               min={this.dateToFormat(new Date)}
+                               onChange={this.onDateChosen}
+                               required
+                        />
+                    }
+
+                </FormLayout>
+
+                <FixedLayout vertical="bottom">
+                    <Div>
+                        <Button size="xl" stretched>Далее</Button>
+                    </Div>
+                </FixedLayout>
+            </Panel>
+        );
+    }
+}
