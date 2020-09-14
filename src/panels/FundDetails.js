7cd3222a4a9bb4e834cbe1f9f2066013eaff83ee
Igor Anokhin <igr.anokhin@gmail.com>
2020-09-14 03:32:37 +0300

    src/panels/FundDetails.js: remove View

--- src/panels/FundDetails.js
+++ src/panels/FundDetails.js
@@ -106,64 +106,62 @@ class FundDetails extends React.Component {
 
   render() {
     return (
-      <View id={this.props.id} header={false}>
-        <Panel>
-          <img src={cat} height="140" style={{display: 'block', width: '100%', objectFit: 'cover'}}></img>
-          <Group separator="show">
-            <Div>
-              <Title level="1" weight="bold" style={{ marginBottom: 4 }}>Добряши помогают котикам</Title>
-              <Headline weight="medium" style={{ marginBottom: 4 }}>Headline medium</Headline>
-              <Text weight="regular">Text regular</Text>
-            </Div>
-          </Group>
-          <Group separator="show">
-            <Div>
-              <Text weight="regular" style={{ marginBottom: 6 }}>Text regular</Text>
-              
-              <div id='progress-outer-div' className='fund-details__progress-outer'>
-                <Text
-                  id='current-text-outer'
-                  weight="regular"
-                  style={{position: 'absolute', left: this.outerTextPosition()}}
-                  className={!this.state.isInnerDivTooShort ? 'd-none' : ''}
-                >{this.formattedSum(this.state.progress)}</Text>
-                <Text id='goal-text' weight="regular" className={
-                  this.state.progress === FUND_GOAL ? 'd-none' :
-                  (this.state.isOuterDivTooShort ? 'text-raised' : '')
-                }>{this.formattedSum(FUND_GOAL)}</Text>
-                <div id='progress-inner-div' className='fund-details__progress-inner' style={{
-                  width: this.progressWidth()
-                }}>
-                  <Text id='current-text-inner' weight="regular" className={
-                    this.state.progress === FUND_GOAL ? 'text-center d-block' :
-                    (this.state.isInnerDivTooShort ? 'd-none' : '')
-                  }>
-                    {this.formattedSum(this.state.progress) + (this.state.progress === FUND_GOAL ? ' собраны!' : '')}
-                  </Text>
-                </div>
+      <Panel id={this.props.id}>
+        <img src={cat} height="140" style={{display: 'block', width: '100%', objectFit: 'cover'}}></img>
+        <Group separator="show">
+          <Div>
+            <Title level="1" weight="bold" style={{ marginBottom: 4 }}>Добряши помогают котикам</Title>
+            <Headline weight="medium" style={{ marginBottom: 4 }}>Headline medium</Headline>
+            <Text weight="regular">Text regular</Text>
+          </Div>
+        </Group>
+        <Group separator="show">
+          <Div>
+            <Text weight="regular" style={{ marginBottom: 6 }}>Text regular</Text>
+            
+            <div id='progress-outer-div' className='fund-details__progress-outer'>
+              <Text
+                id='current-text-outer'
+                weight="regular"
+                style={{position: 'absolute', left: this.outerTextPosition()}}
+                className={!this.state.isInnerDivTooShort ? 'd-none' : ''}
+              >{this.formattedSum(this.state.progress)}</Text>
+              <Text id='goal-text' weight="regular" className={
+                this.state.progress === FUND_GOAL ? 'd-none' :
+                (this.state.isOuterDivTooShort ? 'text-raised' : '')
+              }>{this.formattedSum(FUND_GOAL)}</Text>
+              <div id='progress-inner-div' className='fund-details__progress-inner' style={{
+                width: this.progressWidth()
+              }}>
+                <Text id='current-text-inner' weight="regular" className={
+                  this.state.progress === FUND_GOAL ? 'text-center d-block' :
+                  (this.state.isInnerDivTooShort ? 'd-none' : '')
+                }>
+                  {this.formattedSum(this.state.progress) + (this.state.progress === FUND_GOAL ? ' собраны!' : '')}
+                </Text>
               </div>
-            </Div>
-          </Group>
-          <Group separator="show">
-            <Div>
-              <Text weight="regular">
-                Привет-привет, добряш!
-              </Text>
-            </Div>
-          </Group>
-          <Group separator="show">
-            <Div>
-              <Text weight="regular">
-                Привет-привет, добряш!
-              </Text>
-            </Div>
-          </Group>
+            </div>
+          </Div>
+        </Group>
+        <Group separator="show">
+          <Div>
+            <Text weight="regular">
+              Привет-привет, добряш!
+            </Text>
+          </Div>
+        </Group>
+        <Group separator="show">
+          <Div>
+            <Text weight="regular">
+              Привет-привет, добряш!
+            </Text>
+          </Div>
+        </Group>
+        <Separator wide />
+        <FixedLayout vertical="bottom">
           <Separator wide />
-          <FixedLayout vertical="bottom">
-            <Separator wide />
-          </FixedLayout>
-        </Panel>
-      </View>
+        </FixedLayout>
+      </Panel>
     )
   }
 }
