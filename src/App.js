6091dd93a86528587be69e2644a8ba207946c7fe
Igor Anokhin <igr.anokhin@gmail.com>
2020-09-14 03:04:43 +0300

    srcL App: return default defaultPanel

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends React.Component {
         super(data)
 
         this.state = {
-            activePanel: "fund-details",
+            activePanel: "home",
             history: []
         };
 
