450f8011947bc9e6d3be9ce8e551e7a9a304f409
Igor Anokhin <igr.anokhin@gmail.com>
2020-09-14 03:08:30 +0300

    panels: Home: return Text

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -4,15 +4,15 @@ import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
-// import Text from '@vkontakte/vkui/dist/components/Typography/Text/Text';
+import Text from '@vkontakte/vkui/dist/components/Typography/Text/Text';
 
 const Home = ({ id, go }) => (
     <Panel id={id} centered>
         <PanelHeader>Пожертвования</PanelHeader>
         <Div style={{textAlign: "center"}}>
             <Div>
-                {/* <Text weight="regular">У Вас пока нет сборов.</Text> */}
-                {/* <Text weight="regular">Начните доброе дело.</Text> */}
+                <Text weight="regular">У Вас пока нет сборов.</Text>
+                <Text weight="regular">Начните доброе дело.</Text>
             </Div>
             <Div>
                 <Button size="l" onClick={go} data-to="type-choose">Создать Сбор</Button>
